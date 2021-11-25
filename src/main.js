ed5a9e5ff01a52aec33d6ed12ea5e50f9eb38676
AnaelBonnafous <anael.bonnafous@gmail.com>
2021-11-25 15:19:56 +0100

    fix(icon set): change from MaterialDesginIcons to FontAwesomeV5

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import router from "@/router";
 app.use(router);
 
 import { Quasar } from "quasar";
-import "@quasar/extras/material-icons/material-icons.css";
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
 import "quasar/src/css/index.sass";
 app.use(Quasar, {
   plugins: {},
