55945b8986319f5aaf0fa323f3456b4186d0c4e7
AnaelBonnafous <anael.bonnafous@gmail.com>
2021-11-25 20:03:39 +0100

    fix(layout): reorganise main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
 const app = createApp(App);
 
-import axios from "axios";
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-
-import router from "@/router";
-import '@/assets/styles/global.css';
-
-app.use(router);
-
+// Quasar
 import { Quasar } from "quasar";
 import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
 import "quasar/src/css/index.sass";
 app.use(Quasar, {
   plugins: {},
 });
+
+// Axios
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "https://localhost:8000/api/";
+
+// VueRouter
+import router from "@/router";
+app.use(router);
+
+// Fichiers CSS supplémentaires
+import '@/assets/styles/global.css';
 
 app.mount("#app");
