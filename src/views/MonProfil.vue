fe428a9c7c4f8a90251677427f505e2106770866
AnaelBonnafous <anael.bonnafous@gmail.com>
2021-11-26 11:59:50 +0100

    feature(store): add dynamic authentication data to profile page

--- src/views/MonProfil.vue
+++ src/views/MonProfil.vue
@@ -1,5 +1,16 @@
 <template>
   <q-page class="text-center">
     <h1 class="text-h3">Mon profil</h1>
+    <span class="text-h5 text-white">Adresse email: {{ user.email }}</span>
   </q-page>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters("user", ["user"]),
+  },
+};
+</script>
