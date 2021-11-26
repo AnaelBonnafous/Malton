2db7f168a7f830cd0c35dee74b7348085b358470
AnaelBonnafous <anael.bonnafous@gmail.com>
2021-11-26 11:32:17 +0100

    fix(store): functionnal login method without api call login update store

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -6,18 +6,18 @@ export default {
   }),
 
   actions: {
-    login({ commit, state }, user) {
+    login({ commit }, user) {
       commit("SET_USER", user);
       commit("SET_AUTHENTICATED", true);
     },
   },
 
   mutations: {
-    SET_USER(user) {
+    SET_USER(state, user) {
       state.user = user;
     },
 
-    SET_AUTHENTICATED(authenticated) {
+    SET_AUTHENTICATED(state, authenticated) {
       state.authenticated = authenticated;
     },
   },
