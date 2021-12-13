238f11dbb77dcad8a54ac5799de4117f9e07fbc0
theonicolas19@outlook.com <LightBurning1>
2021-12-13 15:01:15 +0100

    Fix(profil): Fix profile

--- src/views/MonProfil.vue
+++ src/views/MonProfil.vue
@@ -1,11 +1,11 @@
 <template>
   <q-page class="text-center">
-    <h1 class="text-h3 title-profil">Mon profil</h1>
+    <h1 class="text-h3 title-profil q-pa-lg">Mon profil</h1>
     <div class="block-flex-profil">
-      <div class="flex-img">
-        <img src="@/assets/images/layton.jpg" class="image-profil">
+      <div class="flex-img q-ml-lg">
+        <img src="@/assets/images/layton.jpg" class="image-profil radius-50 q-mt-md q-mb-sm">
       </div>
-      <div class="flex-text">
+      <div class="flex-text q-ml-lg">
         <div class="text-h5 text-white text-score"><span>Score total :</span> 850 picarats</div>
         <span class="text-h5 text-white text-mail">{{ user.email }}</span>
         <div class="text-profil text-h5 text-white">
@@ -14,72 +14,87 @@
         </div>
       </div>
     </div>
+    <div class="block-profil q-mb-lg">
+      <div class="block-enigme-fav">
+        <h1 class="text-h3 title-profil q-pa-lg">Enigmes favorites</h1>
+        <q-carousel
+            v-model="slide"
+            transition-prev="slide-right"
+            transition-next="slide-left"
+            swipeable
+            animated
+            control-color="primary"
+            navigation
+            padding
+            arrows
+            class="carousel-enigme-fav shadow-2 rounded-borders"
+        >
+          <q-carousel-slide :name="1" class="column no-wrap">
+            <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
+              <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+              <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide :name="2" class="column no-wrap">
+            <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
+              <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+              <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide :name="3" class="column no-wrap">
+            <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
+              <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+              <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide :name="4" class="column no-wrap">
+            <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
+              <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+              <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
+      </div>
 
-    <div class="block-enigme-fav">
-      <h1 class="text-h3 title-profil">Enigmes favorites</h1>
-      <q-carousel
-          v-model="slide"
-          transition-prev="slide-right"
-          transition-next="slide-left"
-          swipeable
-          animated
-          control-color="primary"
-          navigation
-          padding
-          arrows
-          class="carousel-enigme-fav shadow-2 rounded-borders"
-      >
-        <q-carousel-slide :name="1" class="column no-wrap">
-          <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-            <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
-            <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+      <div class="block-score-diff">
+        <h1 class="text-h3 title-profil q-pa-lg">Score par difficulté</h1>
+        <div class="block-flex-circular">
+          <div>
+            <q-circular-progress
+                show-value
+                :value="value.easy"
+                class="q-ma-md"
+                size="200px"
+                color="a73808"
+            >
+              {{ value.easy }} %
+            </q-circular-progress>
           </div>
-        </q-carousel-slide>
-        <q-carousel-slide :name="2" class="column no-wrap">
-          <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-            <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
-            <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+          <div>
+            <q-circular-progress
+                show-value
+                :value="value.intermediate"
+                class="q-ma-md"
+                size="200px"
+                color="a73808"
+            >
+              {{ value.intermediate }} %
+            </q-circular-progress>
           </div>
-        </q-carousel-slide>
-        <q-carousel-slide :name="3" class="column no-wrap">
-          <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-            <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
-            <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
+          <div>
+            <q-circular-progress
+                show-value
+                :value="value.hard"
+                class="q-ma-md"
+                size="200px"
+                color="a73808"
+            >
+              {{ value.hard }} %
+            </q-circular-progress>
           </div>
-        </q-carousel-slide>
-        <q-carousel-slide :name="4" class="column no-wrap">
-          <div class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap">
-            <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
-            <img src="src/assets/images/enigmes/contre_la_montre.jpg" />
-          </div>
-        </q-carousel-slide>
-      </q-carousel>
-    </div>
-
-    <div class="block-score-diff">
-      <h1 class="text-h3 title-profil">Score par difficulté</h1>
-      <div class="block-flex-circular">
-        <div>
-          <q-circular-progress
-              show-value
-              class="text-light-blue q-ma-md"
-              :value="30"
-              size="200px"
-              color="light-blue"
-          />
-        </div>
-        <div>
-          <q-circular-progress
-              show-value
-              class="text-light-blue q-ma-md"
-              :value="70"
-              size="200px"
-              color="light-blue"
-          />
         </div>
       </div>
     </div>
-
   </q-page>
 </template>
 
@@ -88,14 +103,16 @@ import { mapGetters } from "vuex";
 import { ref } from 'vue';
 
 export default {
+  data: () => ({
+    value: {
+      easy: 30,
+      intermediate: 50,
+      hard: 10
+    },
+  }),
   computed: {
     ...mapGetters("user", ["user"]),
   },
-  setup () {
-    return {
-      slide: ref('style'),
-    }
-  }
 };
 </script>
 
@@ -104,7 +121,13 @@ export default {
 .block-flex-profil {
   display: flex;
   background-color: #5D5166;
-  border-radius: 20px;
+  border-radius: 20px 20px 0 0;
+  border-bottom: 1px solid #2e2525;
+}
+
+.block-profil {
+  background-color: #544d4d;
+  border-radius: 0 0 20px 20px;
 }
 
 .carousel-enigme-fav {
@@ -117,11 +140,11 @@ export default {
 
 .block-flex-circular > div {
   flex: 1;
+  max-width: 29%;
 }
 
 .image-profil {
-  width: 352px;
-  height: 360px;
+  width: 290px;
 }
 
 .flex-text {
@@ -132,10 +155,12 @@ export default {
 
 .text-profil {
   margin-top: 50px;
+  margin-right: 3%;
 }
 
-.text-score{
+.text-score {
   text-align: right;
+  margin-right: 3%;
 }
 
 .text-score > span {
@@ -144,6 +169,10 @@ export default {
 
 .title-profil {
   text-align: left;
+  margin-top: 0;
 }
 
+.q-circular-progress {
+  color: #A73808 !important;
+}
 </style>
