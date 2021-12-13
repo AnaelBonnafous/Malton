d014faa01ac45b31d5b5f481deef6492644847a7
AnaelBonnafous <anael.bonnafous@gmail.com>
2021-12-13 11:27:37 +0100

    feature(indices) : add floating button to display indices on enigme page

--- src/views/Enigme.vue
+++ src/views/Enigme.vue
@@ -1,7 +1,9 @@
 <template>
-  <IntituleEtEnonceEnigme :enigme="enigme"/>
+  <FabIndices v-if="indices.length" :indices="indices" />
 
-  <ReponseSolutionUnique/>
+  <IntituleEtEnonceEnigme :enigme="enigme" />
+
+  <ReponseSolutionUnique />
 
   <pre>
     {{ enigme }}
@@ -10,24 +12,33 @@
 
 <script>
 import IntituleEtEnonceEnigme from "@/components/IntituleEtEnonceEnigme.vue";
-import ReponseSolutionUnique from "@/components/ReponsesEnigmes/ReponseSolutionUnique.vue"
+import ReponseSolutionUnique from "@/components/ReponsesEnigmes/ReponseSolutionUnique.vue";
+import FabIndices from "../components/FabIndices.vue";
 
 export default {
   components: {
     IntituleEtEnonceEnigme,
     ReponseSolutionUnique,
+    FabIndices,
   },
   data: () => ({
     enigmeId: null,
     enigme: null,
+    indices: [],
   }),
-  async created () {
+  async created() {
     this.enigmeId = this.$route.params.enigmeId;
     if (this.enigmeId === undefined) {
-      this.$router.push('difficultes');
+      this.$router.push("difficultes");
     }
     const response = await this.$axios.get("enigmes/" + this.enigmeId);
     this.enigme = response.data;
+
+    this.indices = [
+      this.enigme.indice1,
+      this.enigme.indice2,
+      this.enigme.indice3,
+    ];
   },
 };
 </script>
