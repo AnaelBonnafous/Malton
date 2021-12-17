bfba0dc0d3a988e5b9fba616a097359bc786afed
AnaelBonnafous <anael.bonnafous@gmail.com>
2021-12-17 09:38:20 +0100

    fix(profil) : redisgn profil page + better carousel + responsive design

--- src/views/MonProfil.vue
+++ src/views/MonProfil.vue
@@ -1,93 +1,131 @@
 <template>
-  <q-page class="text-center">
-    <h1 class="text-h3 title-profil q-pa-lg">Mon profil</h1>
-    <div class="block-flex-profil">
-      <div class="flex-img q-ml-lg">
-        <img src="@/assets/images/profil/layton.png" class="image-profil radius-50 q-mt-md q-mb-sm">
-      </div>
-      <div class="flex-text q-ml-lg">
-        <div class="text-h5 text-white text-score"><span>Score total :</span> 850 picarats</div>
-        <span class="text-h5 text-white text-mail">{{ user.data.username }}</span>
-        <div class="text-profil text-h5 text-white">
-          {{ user.data.description }}
-        </div>
-      </div>
-    </div>
-    <div class="block-profil q-mb-lg">
-      <div class="block-enigme-fav">
-        <h1 class="text-h3 title-profil q-pa-lg">Enigmes favorites</h1>
-        <q-carousel
-            v-model="slide"
-            transition-prev="slide-right"
-            transition-next="slide-left"
-            swipeable
-            animated
-            infinite
-            navigation
-            padding
-            navigation-icon="fas fa-circle"
-            prev-icon="fas fa-arrow-left"
-            next-icon="fas fa-arrow-right"
-            control-color="white"
-            arrows
-            class="carousel-enigme-fav"
-        >
-          <q-carousel-slide v-for="(enigme, index) in enigmesFavorites" :name="index" :key="enigme.name"
-                            class="row fit q-gutter-lg items-center no-wrap flex-center q-ma-none">
-            <div class="" style="width: 20%;">
-              <q-img :src="'src/assets/images/enigmes/' + enigme.image_url" class="image-enigme">
-                <div class="absolute-full text-subtitle2 enigme-text text-left column text-body1">
-                  <span class="q-mb-md block text-bold">Nom :
-                    <span class="text-weight-regular">{{ enigme.name }}</span>
-                  </span>
-                  <span class="q-mb-md block text-bold">Description :
-                    <span class="text-weight-regular" v-html="this.$decodeHtml(enigme.brief_description)"></span>
-                  </span>
-                  <span class="q-mb-md block text-bold">Difficulté :
-                    <span class="text-weight-regular">{{ enigme.difficulty.difficulty }}</span>
-                  </span>
-                </div>
-              </q-img>
+  <q-page>
+    <div class="q-my-xl">
+      <div
+        class="row justify-center bg-secondary rounded-borders q-ma-lg q-pa-lg"
+      >
+        <div class="col-12 col-md-4 text-center">
+          <h2 class="text-h3">Mon profil</h2>
+          <q-avatar size="200px" color="primary">
+            <img src="@/assets/images/profil/layton.png" />
+          </q-avatar>
+          <div class="q-mt-md text-h5 text-white">
+            <div>Porte-monnaie : 850 picarats</div>
+            <div>Email : {{ user.username }}</div>
+            <div>
+              <span v-if="user.description">{{ user.description }}</span>
+              <span v-else>Aucune descrption...</span>
             </div>
-          </q-carousel-slide>
-        </q-carousel>
-      </div>
-
-      <div class="block-score-diff">
-        <h1 class="text-h3 title-profil q-pa-lg">Score par difficulté</h1>
-        <div class="block-flex-circular">
-          <div>
-            <q-circular-progress
+          </div>
+        </div>
+        <div class="col-12 col-md-8 text-center">
+          <h2 class="text-h3">Mon score</h2>
+          <div class="row justify-center text-white">
+            <div class="col-12 col-sm-6 col-md-4">
+              <q-circular-progress
                 show-value
                 :value="value.easy"
-                class="q-ma-md"
+                color="green"
                 size="200px"
-                color="a73808"
-            >
-              {{ value.easy }} %
-            </q-circular-progress>
-          </div>
-          <div>
-            <q-circular-progress
+              >
+                {{ value.easy }} %
+              </q-circular-progress>
+              <h3 class="text-h5">Facile</h3>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+              <q-circular-progress
                 show-value
                 :value="value.intermediate"
-                class="q-ma-md"
+                color="orange"
                 size="200px"
-                color="a73808"
-            >
-              {{ value.intermediate }} %
-            </q-circular-progress>
-          </div>
-          <div>
-            <q-circular-progress
+              >
+                {{ value.intermediate }} %
+              </q-circular-progress>
+              <h3 class="text-h5">Intermédiaire</h3>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+              <q-circular-progress
                 show-value
                 :value="value.hard"
-                class="q-ma-md"
+                color="red"
                 size="200px"
-                color="a73808"
+              >
+                {{ value.hard }} %
+              </q-circular-progress>
+              <h3 class="text-h5">Difficile</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row bg-primary rounded-borders q-pa-lg">
+        <div class="col-12 col-md-6 text-center">
+          <h2 class="text-h3">Mes énigmes favorites</h2>
+          <div v-if="enigmesFavorites.length">
+            <q-carousel
+              v-model="slide"
+              thumbnails
+              swipeable
+              infinite
+              animated
+              transition-prev="slide-right"
+              transition-next="slide-left"
+              class="bg-primary"
             >
-              {{ value.hard }} %
-            </q-circular-progress>
+              <q-carousel-slide
+                :img-src="`src/assets/images/enigmes/${enigme.image_url}`"
+                v-for="(enigme, index) in enigmesFavorites"
+                :name="index"
+                :key="index"
+                class="cursor-pointer"
+                @click="playEnigme(enigme.id)"
+              >
+                <q-tooltip
+                  class="bg-secondary rounded-borders"
+                  anchor="center end"
+                  self="center start"
+                  :delay="500"
+                >
+                  <q-card class="text-center text-black">
+                    <img
+                      :src="`src/assets/images/enigmes/${enigme.image_url}`"
+                    />
+                    <q-card-section>
+                      <div class="text-h6">
+                        {{ currentEnigmeFavorite.name }}
+                      </div>
+                      <div class="text-subtitle2">
+                        {{ currentEnigmeFavorite.difficulty.difficulty }}
+                      </div>
+                    </q-card-section>
+                  </q-card>
+                </q-tooltip>
+              </q-carousel-slide>
+            </q-carousel>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 text-center">
+          <h2 class="text-h3">Mes propres énigmes</h2>
+          <div class="row justify-center">
+            <div class="col-6">
+              <p class="text-white">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+                esse, deserunt itaque eaque molestias fuga quidem. Laudantium
+                iure deleniti vero, dolorum corporis, voluptatem magni repellat
+                dolore, recusandae a assumenda sint?
+              </p>
+              <p class="text-white">
+                Necessitatibus, architecto id libero explicabo commodi vitae
+                delectus asperiores quasi repellendus et assumenda provident
+                atque dolor expedita suscipit? A iste qui neque voluptatem
+                laborum maiores harum facere magni beatae modi.
+              </p>
+              <p class="text-white">
+                Porro esse sapiente praesentium mollitia omnis sequi adipisci,
+                id, laudantium fugit rerum a culpa labore nesciunt, tempora
+                inventore nobis laborum repellat explicabo? Incidunt rerum optio
+                reprehenderit, inventore itaque dolorem architecto?
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -96,93 +134,48 @@
 </template>
 
 <script>
-import {mapGetters} from "vuex";
+import { mapGetters } from "vuex";
 
 export default {
   data: () => ({
     slide: 0,
     enigmesFavorites: [],
     value: {
-      easy: 30,
-      intermediate: 50,
-      hard: 10
+      easy: 81,
+      intermediate: 42,
+      hard: 17,
     },
   }),
   computed: {
     ...mapGetters("userStore", ["user"]),
+    currentEnigmeFavorite() {
+      return this.enigmesFavorites[this.slide];
+    },
   },
   async created() {
-    var response = null;
-
-    for (var i = 0; i < this.user.data.enigmeFavorites.length; i++) {
-      response = await this.$axios.get(this.user.data.enigmeFavorites[i].substr(5));
-      this.enigmesFavorites.push(response.data.enigme)
-    }
+    const response = await this.$axios.get(
+      `enigme_favorites?user=${this.user.id}`
+    );
+    this.enigmesFavorites = response.data["hydra:member"].map(
+      (el) => el.enigme
+    );
+  },
+  methods: {
+    playEnigme(id) {
+      this.$router.push({
+        name: "enigme-solution-unique",
+        params: {
+          enigmeId: id,
+        },
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
-
-.block-flex-profil {
-  display: flex;
-  background-color: #5D5166;
-  border-radius: 20px 20px 0 0;
-  border-bottom: 1px solid #2e2525;
-}
-
-.block-profil {
-  background-color: #544d4d;
-  border-radius: 0 0 20px 20px;
-}
-
-.carousel-enigme-fav {
-  background-color: #544d4d;
-}
-
-.block-flex-circular {
-  display: flex;
-}
-
-.block-flex-circular > div {
-  flex: 1;
-  max-width: 29%;
-}
-
-.image-profil {
-  width: 290px;
-}
-
-.flex-text {
-  flex: 1;
-  text-align: left;
-  margin-top: 50px;
-}
-
-.text-profil {
-  margin-top: 50px;
-  margin-right: 3%;
-}
-
-.text-score {
-  text-align: right;
-  margin-right: 3%;
-}
-
-.text-score > span {
-  text-decoration: underline;
-}
-
-.title-profil {
-  text-align: left;
-  margin-top: 0;
-}
-
-.q-circular-progress {
-  color: #A73808 !important;
-}
-
-.enigme {
-  overflow: hidden;
+.q-carousel__slide {
+  background-size: contain;
+  background-repeat: no-repeat;
 }
 </style>
